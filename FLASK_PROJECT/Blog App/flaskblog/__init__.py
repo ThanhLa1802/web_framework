81bc748cb531d9b736afe8cf7ebd591155dcd6c4
your name <lathanhmta@gmail.com>
2021-09-09 17:16:00 +0700

    update code

--- FLASK_PROJECT/Blog App/flaskblog/__init__.py
+++ FLASK_PROJECT/Blog App/flaskblog/__init__.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bcrypt import Bcrypt
-
+from flask_dance.contrib.github import make_github_blueprint, github
 app = Flask(__name__)
 app.secret_key = "0dcea6a90267ed0040313a4616b3a15e"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -15,4 +15,9 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category ='info'
 
+blueprint = make_github_blueprint(
+    client_id="9cc07b32d6aacf29d4b4",
+    client_secret="d7b4f3655831ff69f13d0684e6c0eea7a724a359",
+)
+app.register_blueprint(blueprint, url_prefix="/login")
 from flaskblog import routes
--- FLASK_PROJECT/Blog App/flaskblog/forms.py
+++ FLASK_PROJECT/Blog App/flaskblog/forms.py
@@ -2,18 +2,22 @@ from flaskblog.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.fields.core import BooleanField
-from wtforms.fields.simple import PasswordField, SubmitField
+from wtforms.fields.simple import PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
+from flask_wtf.file import FileAllowed, FileField
+from flask_login import current_user
+
+
 
 class RegistrationForm(FlaskForm):
-    user_name = StringField('Username', validators = [DataRequired(), Length(min =2, max = 20)])
+    username = StringField('Username', validators = [DataRequired(), Length(min =2, max = 20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_user_name(self, user_name):
-        user = User.query.filter_by(user_name=user_name.data).first()
+    def validate_username(self, username):
+        user = User.query.filter_by(user_name=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
@@ -26,3 +30,26 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(min =2, max = 20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.user_name:
+            user = User.query.filter_by(user_name=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators = [DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField('Post')
