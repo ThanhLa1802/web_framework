944c360a37e884882af0620de336783b795ab324
your name <lathanhmta@gmail.com>
2021-09-20 17:39:04 +0700

    update code

--- SQLAlchemy/ORM/Library/library/models.py
+++ SQLAlchemy/ORM/Library/library/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from .extension import db
 
 class Students(db.Model):
@@ -8,13 +9,14 @@ class Students(db.Model):
     class_name = db.Column(db.String(10))
 
     def __init__(self, name, birth_date, gender, class_name):
-        self.name = name
+        self.name = name 
         self.birth_date = birth_date
-        self.gender = gender
+        self.gender = gender 
         self.class_name = class_name
+
 class Books(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100)) 
+    id  = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
     page_count = db.Column(db.Integer)
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
@@ -24,17 +26,19 @@ class Books(db.Model):
         self.page_count = page_count
         self.author_id = author_id
         self.category_id = category_id
+    
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), unique = True, nullable = False)
-
-    def __init__(self, name):
+    def __init__(self, name) :
         self.name = name
+
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), unique = True, nullable = False)
     def __init__(self, name):
         self.name = name
+
 class Borrows(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
