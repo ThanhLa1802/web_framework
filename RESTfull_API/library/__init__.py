fb416a8decb66703e312375ea7c0a8a6230e42ec
your name <lathanhmta@gmail.com>
2024-08-13 16:30:26 +0700

    Update database URL

--- RESTfull_API/library/__init__.py
+++ RESTfull_API/library/__init__.py
@@ -8,15 +8,16 @@ import os
 
 def create_db(app):
     if not os.path.exists("library/library.db"):
-        db.create_all(app=app)
-        print("Created DB!")
+        with app.app_context():
+            db.create_all()
+            print("Created DB!")
 
 
 def create_app(config_file="config.py"):
     app = Flask(__name__)
+    app.config.from_pyfile(config_file)
     db.init_app(app)
     ma.init_app(app)
-    app.config.from_pyfile(config_file)
     create_db(app)
     app.register_blueprint(books)
     app.register_blueprint(borrow)
